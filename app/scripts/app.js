d26157c6dfbccc7beb253c55bb320a059a6bc31f
Alan Jui <alanjui.1960@gmail.com>
2015-02-13 11:51:47 +0800

    啟始

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myAngularApp
+ * @description
+ * # myAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myAngularApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
