59d19d4bb59e468a6b4fe4a18770be4b1ba2866c
Alan Jui <alanjui.1960@gmail.com>
2015-02-17 09:24:07 +0800

    改善Issue＃1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.bootstrap'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -42,6 +43,6 @@ angular
         controller: 'ContactCtrl'
       })
       .otherwise({
-        redirectTo: '/'
+        //redirectTo: '/'
       });
   });
---
+++ app/scripts/directives/company-profile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name myAngularApp.directive:companyProfile
+ * @description
+ * # companyProfile
+ */
+angular.module('myAngularApp')
+  .directive('companyProfile', function () {
+    return {
+      templateUrl: 'views/company-profile.html',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        //element.text('this is the companyProfile directive');
+        scope.oneAtATime = true;
+
+        scope.status = {
+          isFirstOpen: true,
+          isFirstDisabled: false
+        }
+      }
+    };
+  });
